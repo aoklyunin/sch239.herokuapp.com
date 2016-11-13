09e61429483b4af8763ecd048f9d04430644d497
Alexey Klyunin <aoklyunin@gmail.com>
2016-11-13 17:04:07 +0300

    asaasfa

--- DjangoSch239/mysite/mysite/urls.py
+++ DjangoSch239/mysite/mysite/urls.py
@@ -14,15 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+
 from django.conf.urls import include,url
 from django.contrib import admin
+
+
+admin.autodiscover()
 
 from sworks import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^attempt', include('sworks.urls')),
     url(r'^addAttempt$', views.addAttempt, name='addAttempt'),
     url(r'^attemptList', views.attemptList, name='attemptList'),
     url(r'^', views.index, name='index'),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
