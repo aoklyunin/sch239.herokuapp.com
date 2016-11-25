9636c7b128d7a8219c4a6b5b13fb767e5588c04e
Alexey Klyunin <aoklyunin@gmail.com>
2016-11-25 14:25:48 +0300

    v1.0

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     url(r'^register/$', sworks.auth.register),
     url(r'^attempt/(?P<attempt_id>[0-9]+)/$', sworks.views.attempt),
     url(r'^attempt/success/(?P<attempt_id>[0-9]+)/$', sworks.views.success),
+    url(r'^attempt/drop/(?P<attempt_id>[0-9]+)/$', sworks.views.drop),
     url(r'^personal/$', sworks.views.personal),
     url(r'^addTask/$', sworks.views.addTask),
     url(r'^getTasks/$', sworks.views.getTasks),
