1a4f38419f44daf233ee58b7a940b59bc2bce499
Alexey Klyunin <aoklyunin@gmail.com>
2016-12-06 18:05:51 +0300

    wqe

--- sworks/management/commands/checkCode.py
+++ sworks/management/commands/checkCode.py
@@ -13,6 +13,30 @@ from sworks.models import Student, Task, Mark, TaskType, ProgramCode, CodeLangua
 
 # описание класса програмы
 class Command(BaseCommand):
+    # получаем двумерный массив первая координата - оценка, вторая - номер программы в оценке
+    # возвращает массив и длину строк в нём
+    def generateCodeDataFromMarks(self,marks):
+        data = []
+        ln = 0
+        for m in marks:
+            #m.checked = True
+            m.save()
+            ar = []
+            for source in m.sources.all():
+                ar.append(SimpleCodeAnalysis(source))
+            l = len(ar)
+            if ln == 0 and l != 0:
+                ln = l
+            data.append(ar)
+        # если программных кодов нет, то заполняем исходники пустыми строками
+        for d in data:
+            if (len(d)) == 0:
+                for i in range(ln):
+                    d.append(
+                        SimpleCodeAnalysis(ProgramCode.objects.create(language=CodeLanguage.objects.all().first(),
+                                                                      text="")))
+        return (data,ln)
+
     # описание программы
     helf = 'checkCodes'
 
@@ -20,39 +44,31 @@ class Command(BaseCommand):
         # экземпляр класса для работы с mdl
         moodle = MoodleHelper()
         # проходим по всем заданиям за последние 30 дней
+
         tt = TaskType.objects.get(name="Программирование")
         for task in Task.objects.filter(task_type=tt).filter(pub_date__gt=datetime.date.today() - datetime.timedelta(days=30)):
             # for task in Task.objects.all():
                 print(task.task_name)
             #try:
+                marks =  Mark.objects.filter(checked=False).filter(task = task)
+                print(marks)
+                (add_data, ln) = self.generateCodeDataFromMarks(marks)
+                if (len(add_data) == 0):
+                    continue
                 # надо добавить фильтр по непроверенным оценкам
-                marks = Mark.objects.filter(task = task).filter(checked=False)
-                data = []
-                ln = 0
-                for m in marks:
-                    m.checked = True
-                    m.save()
-                    ar = []
-                    for source in m.sources.all():
-                        ar.append(SimpleCodeAnalysis(source))
-                    l = len(ar)
-                    if ln==0 and l!= 0:
-                        ln = l
-                    data.append([ar,m])
-
-                for d in data:
-                    if (len(d[0]))==0:
-                        for i in range(ln):
-                            d[0].append(SimpleCodeAnalysis(ProgramCode.objects.create(language=CodeLanguage.objects.all().first(),
-                                                                                      text = ""  )))
+                marks = Mark.objects.filter(task = task)
+                (all_data,ln) = self.generateCodeDataFromMarks(marks)
 
                 for i in range(ln):
                     arr = []
-                    for j in range(len(data)):
-                        arr.append(data[j][0][i])
-                    #print (arr)
+                    for j in range(len(all_data)):
+                        arr.append(all_data[j][i])
+                    arr_add = []
+                    for j in range(len(add_data)):
+                        arr_add.append(add_data[j][i])
                     dict = {}
-                    for a,b in itertools.combinations(arr,2):
+                    for a,b in itertools.product(arr_add,arr):
+                        if a!=b:
                             c = a.compaireTo(b)
                             #print(c)
                             if c>85:
@@ -75,6 +91,7 @@ class Command(BaseCommand):
                         student = Student.objects.filter(marks=m1).first()
                         mainP = PretendVal.objects.create(programCode = key.sorceCode,mark = m1,student = student)
                         mainP.save()
+
                         pVal = PretendToCheat.objects.create(task = task,state=0,n=value[0][2])
                         pVal.save()
                         pVal.vals.add(mainP)
